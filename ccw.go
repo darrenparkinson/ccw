d15a13931803d36a6ed14a92d1e64eaf945d50cf
Darren Parkinson <dparkinson@natilik.com>
2022-06-21 10:57:08 +0100

    fix: issue where cisco returns non utf8 characters in the response

--- ccw.go
+++ ccw.go
@@ -1,17 +1,21 @@
 package ccw
 
 import (
+	"bufio"
 	"context"
 	"embed"
 	"encoding/json"
 	"encoding/xml"
 	"errors"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"strings"
 	"sync"
 	"time"
+	"unicode"
+	"unicode/utf8"
 
 	"golang.org/x/time/rate"
 )
@@ -127,7 +131,8 @@ func (c *Client) makeXMLRequest(ctx context.Context, req *http.Request, v interf
 	if res.StatusCode == http.StatusCreated {
 		return nil
 	}
-	if err = xml.NewDecoder(res.Body).Decode(&v); err != nil {
+	body := NewValidUTF8Reader(res.Body)
+	if err = xml.NewDecoder(body).Decode(&v); err != nil {
 		return err
 	}
 	return nil
@@ -230,4 +235,36 @@ func FloatOrNil(v float64) *float64 {
 		return &v
 	}
 	return nil
+}
+
+// ValidUTF8Reader implements a Reader which reads only bytes that constitute valid UTF-8
+type ValidUTF8Reader struct {
+	buffer *bufio.Reader
+}
+
+// Function Read reads bytes in the byte array b. n is the number of bytes read.
+func (rd ValidUTF8Reader) Read(b []byte) (n int, err error) {
+	for {
+		var r rune
+		var size int
+		r, size, err = rd.buffer.ReadRune()
+		if err != nil {
+			return
+		}
+		if r == unicode.ReplacementChar && size == 1 {
+			continue
+		} else if n+size < len(b) {
+			utf8.EncodeRune(b[n:], r)
+			n += size
+		} else {
+			rd.buffer.UnreadRune()
+			break
+		}
+	}
+	return
+}
+
+// NewValidUTF8Reader constructs a new ValidUTF8Reader that wraps an existing io.Reader
+func NewValidUTF8Reader(rd io.Reader) ValidUTF8Reader {
+	return ValidUTF8Reader{bufio.NewReader(rd)}
 }
